dbe53a6837e9a6fd73fced2737b8808da9c87766
Deepak Bharti <deepak.bharti@collegedunia.com>
2020-05-24 18:28:01 +0530

    debug set to false

--- dbadshome/settings.py
+++ dbadshome/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wkj7za1rk9_grzecopo8un5qfu=fzx#@a5+-#yl-(4ie&ewo#a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
