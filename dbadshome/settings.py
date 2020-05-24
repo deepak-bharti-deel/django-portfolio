640169702afcf894c9bdd2058d943d3cb0835e38
Deepak Bharti <deepak.bharti@collegedunia.com>
2020-05-24 18:14:37 +0530

    minor changes

--- dbadshome/settings.py
+++ dbadshome/settings.py
@@ -126,3 +126,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
+
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
