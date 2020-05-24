bf3897a06af71f54b507ceebf67fdfdcddb89841
Deepak Bharti <deepak.bharti@collegedunia.com>
2020-05-24 19:02:46 +0530

    fixed static

--- dbadshome/settings.py
+++ dbadshome/settings.py
@@ -122,7 +122,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"), 
+# ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
