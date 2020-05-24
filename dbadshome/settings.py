3603fc5bd54685d6b1a2066ae650dc3b22b7d54b
Deepak Bharti <deepak.bharti@collegedunia.com>
2020-05-24 18:54:57 +0530

    refactor

--- dbadshome/settings.py
+++ dbadshome/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
