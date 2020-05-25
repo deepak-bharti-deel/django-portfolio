8b711165c001dba84da55d2a74fc445990737efc
Deepak Bharti <deepak.bharti@collegedunia.com>
2020-05-26 02:32:08 +0530

    fix

--- dbadshome/settings.py
+++ dbadshome/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"), 
